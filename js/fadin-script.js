1d00de1f8707132b39d2a9ad15d405e020e89258
dujf <king54732@163.com>
2018-06-18 15:50:07 +0800

    新的博客

--- js/fadin-script.js
+++
@@ -1,8 +0,0 @@
-
-/*======= Fade effect =======*/
-jQuery(".features").delegate(".features ul li", "mouseout mouseover", function(m) {
-	if (m.type == 'mouseover') {
-		jQuery(".features ul li").not(this).dequeue().animate({opacity: 0.3}, 400);
-	} else {
-		jQuery(".features ul li").not(this).dequeue().animate({opacity: 1}, 400);}
-});
--- js/hover-image.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".lightbox-image").append("<span></span>");$(".lightbox-image").hover(function(){$(this).find("img").stop().animate({opacity:0.5},"normal")},function(){$(this).find("img").stop().animate({opacity:1},"normal")})})
